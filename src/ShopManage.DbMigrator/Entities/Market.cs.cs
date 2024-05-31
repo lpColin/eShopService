5acc1e3916add84ec56f6a6b491c23fa240082fe
lpColin <mylupeng@163.com>
2024-05-31 14:45:19 +0800

    添加市场类

---
+++ src/ShopManage.DbMigrator/Entities/Market.cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace ShopManage.DbMigrator.Entities
+{
+    public class Market : BasicAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// 标提
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string IconUrl { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public string[] ImgUrls { get; set; }
+    }
+}
